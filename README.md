b36c0650e1ed3f0fd12b57644fd4462b9a1eb841
Vinicius Zaccani <vinicius.zaccani@gmail.com>
2024-11-03 18:40:49 -0300

    Logica finalizada
    Correção de bugs na logica do programa; Criado botão de reset;  readme melhorado.

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-Tip Calculator App
+# Tip Calculator App
 
 Este é um projeto de calculadora de gorjetas desenvolvido como parte de um desafio do [Frontend Mentor](https://www.frontendmentor.io).
+
+## Descrição
+
+O objetivo deste projeto é praticar a manipulação do DOM, oferecendo uma interface intuitiva para calcular gorjetas. A aplicação permite que os usuários insiram o valor total da conta, selecionem uma porcentagem de gorjeta e definam o número de pessoas para dividir a conta. O aplicativo, então, exibe na tela:
+
+- O valor da gorjeta que cada pessoa deve pagar.
+- O valor total que cada pessoa pagará, incluindo a gorjeta.
+
+
+## Tecnologias Utilizadas
+
+- **HTML**: Estrutura da aplicação.
+- **CSS**: Estilização e layout da interface do usuário.
+- **JavaScript**: Lógica da aplicação e manipulação do DOM.
+
 
 
 ## Status do Projeto
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var somaTotal = {
     valor: 0, 
     gorjeta: 0,
@@ -8,59 +7,75 @@ var somaTotal = {
     valorGorjetaUnitario: 0
 }
 
-function buscaValor(Id) {
-    if(Id == 'bill'){
-         somaTotal.valor = window.document.getElementById('bill').value;
-    } else {
-        somaTotal.quantPessoas = window.document.getElementById('numberPeople').value;
+//Preenche a variavel com o valor digitado pelo usuário.
+function buscaValor(id) {
+    if(id === 'bill'){
+         somaTotal.valor = parseFloat(window.document.getElementById('bill').value);
+    } else if (id === 'numberPeople') {
+        somaTotal.quantPessoas = parseInt(window.document.getElementById('numberPeople').value);
     }
     calculaTotal();
 }
 
+// Preeche a variavel e altera o css do botão selecionado.
 function selectTip(valor) {
     clearStyleTips(valor);
     if(valor == 'tip-input'){
-        somaTotal.gorjeta = document.getElementById("tip-input").value;
+        somaTotal.gorjeta = parseFloat(document.getElementById("tip-input").value);
         document.getElementById("tip-input").style.border = "2px  solid hsl(172, 67%, 45%)";
     } else {
-        somaTotal.gorjeta = valor;
+        somaTotal.gorjeta = parseFloat(valor);
         document.getElementById(valor).style.backgroundColor = "hsl(172, 67%, 45%)";
         document.getElementById(valor).style.color= "hsl(183, 100%, 15%)";
     }
     calculaTotal();
 }
 
+
+//Retirando o css especial dos botões
 function clearStyleTips(valor){
-    if(valor != 'tip-input'){
+    if(valor !== 'tip-input'){
         document.getElementById("tip-input").style.border = "0px";
         document.getElementById("tip-input").value = null;
     }
-    document.getElementById("5").style.backgroundColor = "hsl(183, 100%, 15%)";
-    document.getElementById("5").style.color = "white";
 
-    document.getElementById("10").style.backgroundColor = "hsl(183, 100%, 15%)";
-    document.getElementById("10").style.color = "white";
-
-    document.getElementById("15").style.backgroundColor = "hsl(183, 100%, 15%)";
-    document.getElementById("15").style.color = "white";
-
-    document.getElementById("25").style.backgroundColor = "hsl(183, 100%, 15%)";
-    document.getElementById("25").style.color = "white";
-
-    document.getElementById("50").style.backgroundColor = "hsl(183, 100%, 15%)";
-    document.getElementById("50").style.color = "white";
+    ["5", "10", "15", "25", "50"].forEach(numero => {
+        document.getElementById(numero).style.backgroundColor = "hsl(183, 100%, 15%)";
+        document.getElementById(numero).style.color = "white";
+    })
 }
+
+//Calcula o valor da total e exibe na tela.
 function calculaTotal(){
     if(somaTotal.valor == 0 || somaTotal.quantPessoas == 0){
         return;
     }
-    somaTotal.gorjeta = (somaTotal.gorjeta * somaTotal.valor) / 100;
-    somaTotal.valorGorjetaUnitario = (somaTotal.gorjeta / somaTotal.quantPessoas);
-    somaTotal.valorTotal = (Number(somaTotal.valor) + Number(somaTotal.gorjeta));
+
+    const totalGorjeta = (somaTotal.gorjeta * somaTotal.valor) / 100;
+    somaTotal.valorGorjetaUnitario = (totalGorjeta / somaTotal.quantPessoas);
+
+    somaTotal.valorTotal = somaTotal.valor + totalGorjeta;
     somaTotal.valorUnitario = somaTotal.valorTotal / somaTotal.quantPessoas;
 
-    document.getElementById("tip-amount").innerHTML = somaTotal.valorGorjetaUnitario;
-    document.getElementById("total-value").innerHTML = somaTotal.valorUnitario;
+    document.getElementById("tip-amount").innerHTML = `R$${somaTotal.valorGorjetaUnitario.toFixed(2)}`;
+    document.getElementById("total-value").innerHTML = `R$${somaTotal.valorUnitario.toFixed(2)}`;
 }   
 
-//falta alterações de CSS
+//Botão de reset.
+function reset(){
+    somaTotal.valor = 0
+    somaTotal.gorjeta = 0
+    somaTotal.quantPessoas = 0
+    somaTotal.valorTotal = 0
+    somaTotal.valorUnitario = 0
+    somaTotal.valorGorjetaUnitario = 0
+
+    clearStyleTips('')
+
+    document.getElementById("tip-amount").innerHTML = `R$0`;
+    document.getElementById("total-value").innerHTML = `R$0`;
+
+    window.document.getElementById('bill').value = null;
+    window.document.getElementById('numberPeople').value = null;
+
+}
